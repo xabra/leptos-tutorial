08c7eb4fb087d636ed2a0a1d871c7a3d31f30f3d
Adam Brailove <adam.brailove@gmail.com>
2025-06-28 08:12:31 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use leptos::prelude::*;
+
+fn main() {
+    // leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(App);
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button
+            on:click=move |_| set_count.set(3)
+        >
+            "Click me: "
+            {count}
+        </button>
+        <p>
+            "Double count: "
+            {move || count.get() * 2}
+        </p>
+    }
+}
